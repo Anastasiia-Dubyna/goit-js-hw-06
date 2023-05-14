ae883eb2281120a66fed36c6034fa2f9ce594bd1
Anastasiia-Dubyna <122117575+Anastasiia-Dubyna@users.noreply.github.com>
2023-05-14 13:29:03 +0300

    fin

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,25 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", loginFunction);
+
+function loginFunction(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password }
+  } = loginForm;
+
+  if (email.value === "" || password.value === "") {
+    alert("Please fill in the login and/or password fields!");
+    return;
+  }
+
+let userData = {
+    Login: email.value,
+    Password: password.value,
+  };
+
+  console.log(userData);
+
+  loginForm.reset();
+}    
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,18 @@
+const colorOutput = document.querySelector(".color");
+const changeColorButton = document.querySelector(".change-color");
+
+changeColorButton.addEventListener("click", changeColorFunction);
+
+function changeColorFunction() {
+  const randomHexColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomHexColor;
+  colorOutput.innerText = randomHexColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
